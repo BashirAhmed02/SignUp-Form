1609a208d08486e214eec9f2c71ceab1f8012ae0
Bashir <bashir.a.khan02@gmail.com>
2024-05-17 07:27:03 +0500

    Assessment Completed

---
+++ script.js
@@ -0,0 +1,5 @@
+    document.getElementById('signup-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        alert('Form submitted successfully!');
+        // Add more form validation or submission logic here
+    });
